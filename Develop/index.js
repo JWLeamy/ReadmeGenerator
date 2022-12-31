1e61f22a935982ac18a5a192585ad2b88640cb88
John Leamy <jwleamy@gmail.com>
2022-12-30 17:30:11 -0800

    generates readme properly

--- Develop/index.js
+++ Develop/index.js
@@ -26,57 +26,73 @@ ${UserInfo}
 
 ${Tests}
 
+## Contributions
+
+${UserContributions}
+
+## Questions
+
+If you have any questions regarding the project, open an issue or contact me directly at ${Email}. Please Checkout my other projects at ${Github}!
 `
 
 
-function answer() {
-    return [{
-        type: 'input',
-        name: 'Github',
-        message: questions[0],
-        default: 'Github'
-    }, {
-        type: 'input',
-        name: 'Email',
-        message: questions[1],
-        default: 'Email',
-    },{
-        type: 'input',
-        name: 'ProjectName',
-        message: questions[2],
-        default: 'ProjectName'
-    }, {
-        type: 'input',
-        name: 'Description',
-        message: questions[3],
-        default: 'Description'
-    }, {
-        type: 'list',
-        name: 'Liscense',
-        message: `${questions[4]}`,
-        choices: ['MIT', "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
-    }, {
-        type: 'input',
-        name: 'Dependencies',
-        message: questions[5],
-        default: 'Dependencies'
-    }, {
-        type: 'input',
-        name: 'Tests',
-        message: questions[6],
-        default: 'Tests'
-    }, {
-        type: 'input',
-        name: 'UserInfo',
-        message: questions[7],
-        default: 'UserInfo'
-    }, {
-        type: 'input',
-        name: 'UserContributions',
-        message: questions[8],
-        default: 'UserContributions'
-    }];
-}
+inquirer
+  .prompt([
+        {
+            type: 'input',
+            name: 'Github',
+            message: questions[0],
+            default: 'Github'
+        }, {
+            type: 'input',
+            name: 'Email',
+            message: questions[1],
+            default: 'Email',
+        },{
+            type: 'input',
+            name: 'ProjectName',
+            message: questions[2],
+            default: 'ProjectName'
+        }, {
+            type: 'input',
+            name: 'Description',
+            message: questions[3],
+            default: 'Description'
+        }, {
+            type: 'list',
+            name: 'Liscense',
+            message: `${questions[4]}`,
+            choices: ['MIT', "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+        }, {
+            type: 'input',
+            name: 'Dependencies',
+            message: questions[5],
+            default: 'Dependencies'
+        }, {
+            type: 'input',
+            name: 'Tests',
+            message: questions[6],
+            default: 'Tests'
+        }, {
+            type: 'input',
+            name: 'UserInfo',
+            message: questions[7],
+            default: 'UserInfo'
+        }, {
+            type: 'input',
+            name: 'UserContributions',
+            message: questions[8],
+            default: 'UserContributions'
+        }
+    ])
+    .then((answers) => {
+        const htmlPageContent = generateREADME(answers);
+    
+        fs.writeFile('README.md', htmlPageContent, (err) =>
+          err ? console.log(err) : console.log('Successfully created README file!')
+        );
+      }
+    );
 
 // TODO: Create a function to initialize app
 function init() {}
@@ -86,7 +102,7 @@ init();
 
 
 // Not sure of the above instreuction, but this is how I would approach the problem.
-inquirer.prompt(answer()).then(answers => {
+/*inquirer.prompt(answer()).then(answers => {
     fs.writeFile("./README.md", JSON.stringify(answers), function (err) {
         console.log(err)
-    })})
+    })})*/
--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # ReadmeGenerator
-s
